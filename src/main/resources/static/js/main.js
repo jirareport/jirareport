01d0e18a88f54fc3eb567daf226023f78d799c5f
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-05-08 00:35:19 -0300

    melhorando ui

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    $('.nav-tabs').scrollingTabs();
+    $('.nav-tabs').scrollingTabs({
+        disableScrollArrowsOnFullyScrolled: true
+    });
 
     $('.search-input').hideseek({
        highlight: true,
