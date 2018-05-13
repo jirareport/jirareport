aaebaa3b957e3ab292756d9eeefd4e4f2e11c7b7
Júlio Falbo <julio.falbo.rj@gmail.com>
2018-05-13 00:46:08 -0300

    #21 - Utilizar api do jira para sugerir fluxo do projeto (Code Review)

--- src/main/java/br/com/leonardoferreira/jirareport/controller/ProjectController.java
+++ src/main/java/br/com/leonardoferreira/jirareport/controller/ProjectController.java
@@ -3,7 +3,12 @@ package br.com.leonardoferreira.jirareport.controller;
 import br.com.leonardoferreira.jirareport.domain.Project;
 import br.com.leonardoferreira.jirareport.service.ProjectService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
