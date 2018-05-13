f826973038bfb7d3c836e0d697466fced141c73a
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-05-13 17:53:39 -0300

    JIRAT-23 mapstruct

--- src/main/java/br/com/leonardoferreira/jirareport/client/HolidayClient.java
+++ src/main/java/br/com/leonardoferreira/jirareport/client/HolidayClient.java
@@ -1,11 +1,11 @@
 package br.com.leonardoferreira.jirareport.client;
 
+import java.util.List;
+
 import br.com.leonardoferreira.jirareport.domain.vo.HolidayVO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 /**
  * Created by jfalbo
@@ -13,9 +13,8 @@ import java.util.List;
 @FeignClient(name = "holiday-client", url = "${holiday.url} + ${holiday.token}")
 public interface HolidayClient {
 
-    @GetMapping()
+    @GetMapping
     List<HolidayVO> findAllHolidaysInCity(@RequestParam("ano") final String year,
                                           @RequestParam("estado") final String state,
                                           @RequestParam("cidade") final String city);
-
 }
