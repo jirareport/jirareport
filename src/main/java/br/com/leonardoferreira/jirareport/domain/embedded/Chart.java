65e8bc527b298aa938fdc206e5b0a08bada48420
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-05-16 23:21:59 -0300

    WIP migração

--- src/main/java/br/com/leonardoferreira/jirareport/domain/embedded/Chart.java
+++ src/main/java/br/com/leonardoferreira/jirareport/domain/embedded/Chart.java
@@ -8,7 +8,9 @@ import lombok.Data;
 import java.beans.Transient;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author lferreira
@@ -19,33 +21,29 @@ import java.util.List;
 public class Chart<L, V> implements Serializable {
     private static final long serialVersionUID = 7550041573002395950L;
 
-    private List<L> axisX;
-
-    private List<V> axisY;
+    private Map<L, V> data;
 
     public Chart() {
-        this.axisX = new ArrayList<>();
-        this.axisY = new ArrayList<>();
+        data = new HashMap<>();
     }
 
     @Transient
     public String getAxisXJSON() throws JsonProcessingException {
-        return new ObjectMapper().writeValueAsString(axisX);
+        return new ObjectMapper().writeValueAsString(data.keySet());
     }
 
     @Transient
     public String getAxisYJSON() throws JsonProcessingException {
-        return new ObjectMapper().writeValueAsString(axisY);
+        return new ObjectMapper().writeValueAsString(data.values());
     }
 
     @Transient
     public boolean getHasData() {
-        return !axisX.isEmpty() && !axisY.isEmpty();
+        return data != null && !data.keySet().isEmpty();
     }
 
     public void add(final L x, final V y) {
-        axisX.add(x);
-        axisY.add(y);
+        data.put(x, y);
     }
 
 }
