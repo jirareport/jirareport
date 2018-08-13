8695ba0105bb3f79c739afbe9527429d6e19325b
Leonardo Ferreira <s2it_leferreira@uolinc.com>
2018-08-13 12:10:02 -0300

    JIRAT-33 import

--- src/main/java/br/com/leonardoferreira/jirareport/service/impl/IssueServiceImpl.java
+++ src/main/java/br/com/leonardoferreira/jirareport/service/impl/IssueServiceImpl.java
@@ -11,6 +11,7 @@ import br.com.leonardoferreira.jirareport.domain.vo.SandBox;
 import br.com.leonardoferreira.jirareport.domain.vo.SandBoxFilter;
 import br.com.leonardoferreira.jirareport.mapper.IssueMapper;
 import br.com.leonardoferreira.jirareport.repository.IssueRepository;
+import br.com.leonardoferreira.jirareport.service.BoardService;
 import br.com.leonardoferreira.jirareport.service.ChartService;
 import br.com.leonardoferreira.jirareport.service.IssueService;
 import br.com.leonardoferreira.jirareport.service.LeadTimeService;
