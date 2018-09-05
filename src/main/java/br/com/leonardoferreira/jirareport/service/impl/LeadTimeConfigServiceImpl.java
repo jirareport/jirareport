04e02a85185958ce15a0ac18448ed06316d88911
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-09-05 17:55:13 -0300

    JIRAT-73 correção para a alteração de lead times

--- src/main/java/br/com/leonardoferreira/jirareport/service/impl/LeadTimeConfigServiceImpl.java
+++ src/main/java/br/com/leonardoferreira/jirareport/service/impl/LeadTimeConfigServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import br.com.leonardoferreira.jirareport.domain.Board;
 import br.com.leonardoferreira.jirareport.domain.LeadTimeConfig;
+import br.com.leonardoferreira.jirareport.exception.ResourceNotFound;
+import br.com.leonardoferreira.jirareport.mapper.LeadTimeConfigMapper;
 import br.com.leonardoferreira.jirareport.repository.LeadTimeConfigRepository;
 import br.com.leonardoferreira.jirareport.service.LeadTimeConfigService;
 import br.com.leonardoferreira.jirareport.service.BoardService;
@@ -24,6 +26,9 @@ public class LeadTimeConfigServiceImpl extends AbstractService implements LeadTi
 
     @Autowired
     private BoardService boardService;
+
+    @Autowired
+    private LeadTimeConfigMapper leadTimeConfigMapper;
 
     @Override
     @Transactional(readOnly = true)
@@ -54,11 +59,13 @@ public class LeadTimeConfigServiceImpl extends AbstractService implements LeadTi
 
     @Override
     @Transactional
-    public void update(final Long boardId, final LeadTimeConfig leadTimeConfig) {
-        log.info("Method=update, boardId={}, leadTimeConfig={}", boardId, leadTimeConfig);
+    public void update(final Long boardId, final LeadTimeConfig request) {
+        log.info("Method=update, boardId={}, request={}", boardId, request);
 
-        Board board = boardService.findById(boardId);
-        leadTimeConfig.setBoard(board);
+        LeadTimeConfig leadTimeConfig = leadTimeConfigRepository.findById(request.getId())
+                .orElseThrow(ResourceNotFound::new);
+
+        leadTimeConfigMapper.update(leadTimeConfig, request);
 
         leadTimeConfigRepository.save(leadTimeConfig);
     }
