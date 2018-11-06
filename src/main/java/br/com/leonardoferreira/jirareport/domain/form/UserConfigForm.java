8e4a03e65980456fd525cfee2c9ad68f4683ceee
Leonardo Ferreira <mail@leonardoferreira.com.br>
2018-11-06 17:16:54 -0200

    JIRAT-79 (#53)
    * JIRAT-79 permitindo escolher o tipo de gráfico
    
    * JIRAT-79 checkstyle
    
    * JIRAT-79 checkstyle

--- src/main/java/br/com/leonardoferreira/jirareport/domain/form/UserConfigForm.java
+++ src/main/java/br/com/leonardoferreira/jirareport/domain/form/UserConfigForm.java
@@ -4,19 +4,18 @@ import br.com.leonardoferreira.jirareport.util.DateUtil;
 import br.com.leonardoferreira.jirareport.util.StringUtil;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-
 @Data
 public class UserConfigForm {
 
-    @NotBlank(message = "Estado não pode ser vazio.")
     private String state;
 
-    @NotBlank(message = "Cidade não pode ser vazia.")
     private String city;
 
-    @NotBlank(message = "Token não pode ser vazio.")
     private String holidayToken;
+
+    private String leadTimeChartType;
+
+    private String throughputChartType;
 
     public String getCity() {
         if (this.city == null) {
