e443b536bbeddf6e405d4e8b3bd2ab1a3e219d01
Oriol Canalias <s2it_ocanalias@uolinc.com>
2018-11-14 13:32:37 -0200

    Corrigindo data inicial nula

--- src/main/java/br/com/leonardoferreira/jirareport/mapper/EstimateIssueMapper.java
+++ src/main/java/br/com/leonardoferreira/jirareport/mapper/EstimateIssueMapper.java
@@ -23,6 +23,7 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -68,7 +69,9 @@ public class EstimateIssueMapper {
                     if ("BACKLOG".equals(board.getStartColumn())) {
                         startDate = created;
                     }
-
+                    if (startDate == null){
+                        return null;
+                    }
                     String priority = null;
                     if (fields.has("priority") && !fields.get("priority").isNull() && !fields.path("priority").isMissingNode()) {
                         priority = fields.path("priority").get("name").asText(null);
@@ -99,7 +102,9 @@ public class EstimateIssueMapper {
                             .impedimentTime(timeInImpediment)
                             .priority(priority)
                             .build();
-                }).collect(Collectors.toList());
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
     @SneakyThrows
