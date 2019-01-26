6c889af111db119916875213aa3ad86145e6a801
Leonardo Ferreira <mail@leonardoferreira.com.br>
2019-01-26 11:48:22 -0200

    alterando sistema de login e começando migração para rest

--- src/main/java/br/com/leonardoferreira/jirareport/config/security/JiraAuthenticationProvider.java
+++ src/main/java/br/com/leonardoferreira/jirareport/config/security/JiraAuthenticationProvider.java
@@ -1,34 +1,48 @@
-package br.com.leonardoferreira.jirareport.config;
-
-import java.util.Collections;
+package br.com.leonardoferreira.jirareport.config.security;
 
 import br.com.leonardoferreira.jirareport.domain.form.LoginForm;
 import br.com.leonardoferreira.jirareport.domain.vo.Account;
 import br.com.leonardoferreira.jirareport.service.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.jwt.crypto.sign.RsaSigner;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class CustomAuthenticationProvider implements AuthenticationProvider {
+public class JiraAuthenticationProvider implements AuthenticationProvider {
 
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private RsaSigner rsaSigner;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Override
     public Authentication authenticate(final Authentication auth) {
         try {
-            Account login = authService.login(new LoginForm(auth.getName(), auth.getCredentials().toString()));
-            if (login == null) {
+            Account account = authService.login(new LoginForm(auth.getName(), auth.getCredentials().toString()));
+            if (account == null) {
                 throw new BadCredentialsException("External system authentication failed");
             }
 
-            return new UsernamePasswordAuthenticationToken(login, null, Collections.emptyList());
+            Jwt jwt = JwtHelper.encode(objectMapper.writeValueAsString(account), rsaSigner);
+
+            UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(
+                    account, account.getPassword(), account.getAuthorities());
+            user.setDetails(jwt.getEncoded());
+
+            return user;
         } catch (Exception e) {
             log.error("Method=authenticate, E={}", e.getMessage(), e);
             throw new BadCredentialsException(e.getMessage(), e);
---
+++ src/main/java/br/com/leonardoferreira/jirareport/config/security/TokenAuthenticationProvider.java
@@ -0,0 +1,41 @@
+package br.com.leonardoferreira.jirareport.config.security;
+
+import br.com.leonardoferreira.jirareport.domain.vo.Account;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.jwt.crypto.sign.RsaVerifier;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedCredentialsNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private RsaVerifier rsaVerifier;
+
+    @Override
+    public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
+        try {
+            Jwt jwt = JwtHelper.decodeAndVerify((String) authentication.getPrincipal(), rsaVerifier);
+            Account account = objectMapper.readValue(jwt.getClaims(), Account.class);
+            return new PreAuthenticatedAuthenticationToken(account, account.getPassword(), account.getAuthorities());
+        } catch (Exception e) {
+            throw new PreAuthenticatedCredentialsNotFoundException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public boolean supports(final Class<?> authentication) {
+        return authentication.equals(PreAuthenticatedAuthenticationToken.class);
+    }
+
+}
