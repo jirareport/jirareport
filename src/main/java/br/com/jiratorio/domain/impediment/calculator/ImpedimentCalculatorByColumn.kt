747f646add01e0ff0ee3a34db7bac16c4092f326
Leonardo Ferreira <mail@leonardoferreira.com.br>
2019-03-15 23:04:12 -0300

    correct sum

--- src/main/java/br/com/jiratorio/domain/impediment/calculator/ImpedimentCalculatorByColumn.kt
+++ src/main/java/br/com/jiratorio/domain/impediment/calculator/ImpedimentCalculatorByColumn.kt
@@ -1,14 +1,13 @@
 package br.com.jiratorio.domain.impediment.calculator
 
 import br.com.jiratorio.domain.entity.embedded.Changelog
-import br.com.jiratorio.sumByLong
 
 object ImpedimentCalculatorByColumn {
 
-    fun timeInImpediment(changelog: List<Changelog>, impedimentColumns: List<String>): Long {
-        return changelog
-                .filter { impedimentColumns.contains(it.to) }
-                .sumByLong { it.leadTime ?: 0 }
-    }
+    fun timeInImpediment(changelog: List<Changelog>, impedimentColumns: List<String>) =
+            changelog
+                    .filter { impedimentColumns.contains(it.to) }
+                    .map { it.leadTime ?: 0 }
+                    .sum()
 
 }
--- src/main/java/br/com/jiratorio/extension.SumByLong.kt
+++
@@ -1,5 +0,0 @@
-package br.com.jiratorio
-
-inline fun <T> Iterable<T>.sumByLong(selector: (T) -> Long): Long {
-    return map { selector(it) }.sum()
-}
