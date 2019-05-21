fbab9f6eeca03528ecd8512756ed40d5f661d2a6
Leonardo Ferreira <mail@leonardoferreira.com.br>
2019-05-21 13:07:28 -0300

    JIRA-3 Histórico de impedimento

--- src/main/kotlin/br/com/jiratorio/domain/entity/Issue.kt
+++ src/main/kotlin/br/com/jiratorio/domain/entity/Issue.kt
@@ -10,7 +10,6 @@ import java.util.Objects
 import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
