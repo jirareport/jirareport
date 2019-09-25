d1199abac0055f7c6ea1e37a7305bf271f2e5185
Leonardo Ferreira <mail@leonardoferreira.com.br>
2019-09-25 13:18:16 -0300

    JIRA-30 correção para problema de status com barra invertida

--- src/main/kotlin/br/com/jiratorio/extension/StringExtension.kt
+++ src/main/kotlin/br/com/jiratorio/extension/StringExtension.kt
@@ -8,11 +8,9 @@ import java.util.Base64
 fun String.toBase64(): String =
     String(Base64.getEncoder().encode(this.toByteArray()))
 
-fun String.fromJiraToLocalDateTime(): LocalDateTime {
-    return LocalDateTime.parse(this.substring(0, 19), DateTimeFormatter.ISO_DATE_TIME)
-}
+fun String.fromJiraToLocalDateTime(): LocalDateTime =
+    LocalDateTime.parse(this.substring(0, 19), DateTimeFormatter.ISO_DATE_TIME)
 
-fun String.stripAccents(): String {
-    return Normalizer.normalize(this, Normalizer.Form.NFD)
+fun String.stripAccents(): String =
+    Normalizer.normalize(this, Normalizer.Form.NFD)
         .replace("[\\p{InCombiningDiacriticalMarks}]".toRegex(), "")
-}
