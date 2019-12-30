008f8e02b827d1cfe8cc4da4acae2d1217c44c80
Leonardo Ferreira <mail@leonardoferreira.com.br>
2019-12-30 19:24:45 -0300

    JIRA-37 atualizando dependências  (#79)
    * JIRA-37 atualizando detekt
    
    * JIRA-37 atualizando gradle
    
    * JIRA-37 atualizando jvm
    
    * JIRA-37 atualizando hibernate-types-52
    
    * JIRA-37 atualizando org.testcontainers:postgresql

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,15 @@ plugins {
     kotlin("plugin.spring") version "1.3.61"
     kotlin("plugin.jpa") version "1.3.61"
 
-    id("io.gitlab.arturbosch.detekt") version "1.1.1"
+    id("io.gitlab.arturbosch.detekt") version "1.3.1"
 }
 
 apply {
     from("gradle/tasks/CreateMigration.gradle.kts")
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_1_8
-java.targetCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_12
+java.targetCompatibility = JavaVersion.VERSION_12
 
 repositories {
     mavenCentral()
@@ -29,7 +29,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.postgresql:postgresql")
-    implementation("com.vladmihalcea:hibernate-types-52:2.7.1")
+    implementation("com.vladmihalcea:hibernate-types-52:2.8.0")
     implementation("org.flywaydb:flyway-core")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
@@ -48,7 +48,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation("org.testcontainers:postgresql:1.12.3")
+    testImplementation("org.testcontainers:postgresql:1.12.4")
 
     testImplementation("com.github.javafaker:javafaker:1.0.1")
     testImplementation("io.rest-assured:rest-assured")
@@ -61,7 +61,7 @@ dependencies {
 
     testImplementation("io.mockk:mockk:1.9.3")
 
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.1.1")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.3.1")
 }
 
 dependencyManagement {
@@ -77,7 +77,7 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict", "-Xjvm-default=enable")
-        jvmTarget = "1.8"
+        jvmTarget = "12"
     }
 }
 
