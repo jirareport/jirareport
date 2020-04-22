d399fc9d3e134f2577d3d0cb401ffd9446d4513a
Leonardo Ferreira <mail@leonardoferreira.com.br>
2020-04-22 00:31:05 -0300

    GH-88 Arrumando erro ao remover board com issue associadas

--- src/main/kotlin/br/com/jiratorio/domain/entity/Issue.kt
+++ src/main/kotlin/br/com/jiratorio/domain/entity/Issue.kt
@@ -6,7 +6,6 @@ import br.com.jiratorio.extension.toStringBuilder
 import org.hibernate.annotations.Type
 import java.time.LocalDateTime
 import java.util.Objects
-import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -64,7 +63,7 @@ data class Issue(
     @Column(name = "issue_period_id", nullable = false)
     var issuePeriodId: Long = 0,
 
-    @OneToMany(mappedBy = "issue", cascade = [CascadeType.REMOVE])
+    @OneToMany(mappedBy = "issue")
     var leadTimes: MutableSet<LeadTime>? = null,
 
     @ManyToOne(optional = false)
